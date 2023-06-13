a835f00ca07e74b2258275221ef28c32d59f8646
Renan Riberio <renan.ribeiro@flsoft.com.br>
2023-06-13 14:33:54 -0300

    ajuste cad pessoa

--- src/controllers/PessoaController.ts
+++ src/controllers/PessoaController.ts
@@ -14,21 +14,13 @@ class PessoaController {
     }
   }
   public async salvarPessoa(req: Request, res: Response): Promise<Response> {
-    // try {
-    //   var conexao: any = await Conexao.connectDb();
-    // } catch (err) {
-    //   return res.status(500);
-    // }
     try {
-      const { idEmpresa, idPessoa, enderecos, tipoInclusao, nome, cnpjCpf, tipoPessoa, situacao, funcionario, cliente, fornecedor, tipoFuncionario, equipe } = req.body;
-      //const repository = new PessoaRepository(conexao);
+      const { idEmpresa, idPessoa, enderecos, tipoInclusao, nome, cnpjCpf, tipoPessoa, situacao, flagFuncionario, flagCliente, flagFornecedor, tipoFuncionario, equipe, inscricaoMunicipal, rg, inscricaoEstadual } = req.body;
 
       if (tipoInclusao === "I") {
-        await pessoaService.create({ nome, cnpjCpf, enderecos, tipoPessoa, situacao, funcionario, cliente, fornecedor, tipoFuncionario, equipe });
-        // await repository.inserirPessoa(idEmpresa, nome, cnpjCpf, tipoPessoa, situacao, funcionario, cliente, fornecedor, tipoFuncionario, idEquipe);
+        await pessoaService.create({ nome, cnpjCpf, enderecos, tipoPessoa, situacao, flagFuncionario, flagCliente, flagFornecedor, tipoFuncionario, equipe, inscricaoMunicipal, rg, inscricaoEstadual });
       } else if (tipoInclusao === "E") {
-        await pessoaService.update(idPessoa, { nome, cnpjCpf, enderecos, tipoPessoa, situacao, funcionario, cliente, fornecedor, tipoFuncionario, equipe });
-        //await repository.alterarPessoa(idPessoa, idEmpresa, nome, cnpjCpf, tipoPessoa, situacao, funcionario, cliente, fornecedor, tipoFuncionario, equipe);
+        await pessoaService.update(idPessoa, { nome, cnpjCpf, enderecos, tipoPessoa, situacao, flagFuncionario, flagCliente, flagFornecedor, tipoFuncionario, equipe, inscricaoMunicipal, rg, inscricaoEstadual });
       }
 
       return res.sendStatus(204);
@@ -37,16 +29,9 @@ class PessoaController {
     }
   }
   public async excluirPessoa(req: Request, res: Response): Promise<Response> {
-    // try {
-    //   var conexao: any = await Conexao.connectDb();
-    // } catch (err) {
-    //   return res.status(500);
-    // }
     try {
       const requisicao = req.query;
-      // const repository = new PessoaRepository(conexao);
       const idPessoa = requisicao.idPessoa as string;
-      // await repository.excluirPessoa(idPessoa);
       await pessoaService.remove(idPessoa);
 
       return res.sendStatus(204);
